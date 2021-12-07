78efef7f89107b92610fb81ca44be2228d4a2bfb
jaeyeun <jaeyeun95@gmail.com>
2021-12-08 00:23:16 +0900

    java StringBuilder 클래스

---
+++ swJava/src/stringbuilder/StringBuilderEx2.java
@@ -0,0 +1,15 @@
+package stringbuilder;
+
+public class StringBuilderEx2 {
+    public static void main(String[] args) {
+        StringBuilder a = new StringBuilder("hello");
+
+        a.append("World");
+
+        System.out.println(a);
+
+        System.out.println(a.substring(0,1));
+        // a.
+    }
+    
+}
