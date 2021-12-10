806cff711fb6127b1c839ddb75397ec0253500bd
jaeyeun95 <jaeyeun95@gmail.com>
2021-12-10 16:04:28 +0900

    FileOutputStream 주석 추가

--- swJava/src/streamex/FileOutputStreamEx.java
+++ swJava/src/streamex/FileOutputStreamEx.java
@@ -9,11 +9,16 @@ public class FileOutputStreamEx {
     public static void main(String[] args) throws FileNotFoundException, IOException {
         FileOutputStream fos = null;
         try {
+            // 경로
             String path = "D:\\test";
+            // 해당 경로에 파일 객체를 만들고
             File f = new File(path);
+            // 해당 경로에 test 라는 파일이 없으면 만들어주고
             if(!f.exists())
-            f.mkdirs();
+                f.mkdirs();
 
+            // FileOutputStream (String name, boolean append))
+            // name = 파일 시스템의 실제 경로, append 가 true 면 이어쓰기의 기능, false 면 덮어쓰기의 기능
             fos = new FileOutputStream(path+File.separator+"fileout2.txt", true);
             String msg = "HI FILEOUTPUTSTREAM";
             fos.write(msg.getBytes());
