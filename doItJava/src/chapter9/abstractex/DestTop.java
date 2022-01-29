28daaa10c875527708e5f136f26c7cd40c5437f9
jaeyeun <jaeyeun95@gmail.com>
2022-01-29 23:26:58 +0900

    chapter 9 추상클래스 샘플 추상클래스작성

---
+++ doItJava/src/chapter9/abstractex/DestTop.java
@@ -0,0 +1,16 @@
+package chapter9.abstractex;
+
+public class DestTop extends Computer {
+
+    @Override
+    public void display() {
+        System.out.println("DeskTop display()");
+    }
+
+    @Override
+    public void typing() {
+        System.out.println("DeskTop typing()");
+
+    }
+
+}
