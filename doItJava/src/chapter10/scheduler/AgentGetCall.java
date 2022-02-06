54e9fb169effb3fccda69fd307869a4de2580e6a
jaeyeun <jaeyeun95@gmail.com>
2022-02-06 22:24:37 +0900

    10-2. 인터페이스와 다형성 상담원 예제를 통해 다형성 예시 구현

---
+++ doItJava/src/chapter10/scheduler/AgentGetCall.java
@@ -0,0 +1,14 @@
+package chapter10.scheduler;
+
+// 상담원이 본인이 필요할 때 가져오는 정책
+public class AgentGetCall implements Scheduler{
+    @Override
+    public void getNextCall() {
+        System.out.println("상담원이 다음 전화 요청");
+    }
+
+    @Override
+    public void sendCallToAgent() {
+        System.out.println("상담원이 전화 상담을 가져 갔습니다.");
+    }
+}
