d758cc1a1456e81b8e4e1a6d0bb33263b1dd8e72
jaeyeun <jaeyeun95@gmail.com>
2022-02-07 20:25:24 +0900

    chap 10. 연습문제 Q7

---
+++ doItJava/src/chapter10/practice/HeapSort.java
@@ -0,0 +1,19 @@
+package chapter10.practice;
+
+public class HeapSort implements Sort {
+    @Override
+    public void ascending(int[] arr) {
+        System.out.println("HeapSort ascending");
+    }
+
+    @Override
+    public void descending(int[] arr) {
+        System.out.println("HeapSort descending");
+
+    }
+
+    @Override
+    public void description() {
+        System.out.println("HeapSort description");
+    }
+}
---
+++ doItJava/src/chapter10/practice/QuickSort.java
@@ -0,0 +1,19 @@
+package chapter10.practice;
+
+public class QuickSort implements Sort {
+    @Override
+    public void ascending(int[] arr) {
+        System.out.println("QuickSort ascending");
+    }
+
+    @Override
+    public void descending(int[] arr) {
+        System.out.println("QuickSort descending");
+
+    }
+
+    @Override
+    public void description() {
+        System.out.println("QuickSort description");
+    }
+}
