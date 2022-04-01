71e41287a34783270bfd48dc5872e61c411935ca
jaeyeun <jaeyeun95@gmail.com>
2022-04-01 20:20:32 +0900

    try-with-resources 사용2

--- doItJava/src/chapter14/exception/AutoCloseTest.java
+++ doItJava/src/chapter14/exception/AutoCloseTest.java
@@ -1,2 +1,11 @@
-package chapter14.exception;public class AutoCloseTest {
+package chapter14.exception;
+
+public class AutoCloseTest {
+    public static void main(String[] args) {
+        try (AutoCloseObj obj = new AutoCloseObj()){
+            throw new Exception();  // 가엦 에외 발생
+        }catch (Exception e){
+            System.out.println("예외 부분입니다.");
+        }
+    }
 }
