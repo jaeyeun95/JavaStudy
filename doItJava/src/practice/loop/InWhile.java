babe4452e1ffabae7bdb0c485c7d3d3f22955060
jaeyeun <jaeyeun95@gmail.com>
2022-04-21 23:03:16 +0900

    while문 예제 추가

---
+++ doItJava/src/practice/loop/InWhile.java
@@ -0,0 +1,12 @@
+package practice.loop;
+
+public class InWhile {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 10){
+            if (i == 5) return;
+            System.out.println(i);
+            i++;
+        }
+    }
+}
