e465cc642c2fa0a6e55a3b6d34e5e0d4a22275bd
jaeyeun95 <jaeyeun95@gmail.com>
2022-05-18 23:06:44 +0900

    ScannerTest

--- doItJava/src/chapter15/stream/others/ScannerTest.java
+++ doItJava/src/chapter15/stream/others/ScannerTest.java
@@ -1,5 +1,6 @@
 package chapter15.stream.others;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ScannerTest {
@@ -12,15 +13,10 @@ public class ScannerTest {
         String job = sc.nextLine();
         System.out.println("사번 : ");
         int num;
-        try {
-            num = sc.nextInt();
-            System.out.println(name);
-            System.out.println(job);
-            System.out.println(num);
-
-        } catch (Exception e){
-            System.out.println(e.getMessage());
-        }
+        num = sc.nextInt();
+        System.out.println(name);
+        System.out.println(job);
+        System.out.println(num);
 
 
     }
