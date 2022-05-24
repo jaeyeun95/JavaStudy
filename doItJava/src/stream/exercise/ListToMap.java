093c1ff1c086d39e177f20fcd41326a71bed7f2a
jaeyeun95 <jaeyeun95@gmail.com>
2022-05-24 23:01:30 +0900

    stream List -> Map

---
+++ doItJava/src/stream/exercise/ListToMap.java
@@ -0,0 +1,23 @@
+package stream.exercise;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class ListToMap {
+    public static void main(String[] args) {
+        List<Person> personList = new ArrayList<>();
+        personList.add(new Person("짱구", 23, "010-1234-5666"));
+        personList.add(new Person("유리", 24, "010-1234-5677"));
+        personList.add(new Person("철수", 25, "010-1234-5699"));
+        personList.add(new Person("맹구", 26, null));
+
+        // Function.identity 는 t -> t, 항상 입력된 인자(자신)을 반환한다.
+        Map<String, Person> personMap = personList.stream()
+                .collect(Collectors.toMap(Person::getName, Function.identity()));
+
+        System.out.println("personMap : " + personMap.toString());
+    }
+}
---
+++ doItJava/src/stream/exercise/Person.java
@@ -0,0 +1,51 @@
+package stream.exercise;
+
+import jdk.jfr.DataAmount;
+
+import java.lang.annotation.Target;
+
+// https://madplay.github.io/post/java-streams-examples 참고 페이지
+public class Person {
+    private String name;
+    private int age;
+    private String phoneNumber;
+
+    public Person(String name, int age, String phoneNumber) {
+        this.name = name;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+}
