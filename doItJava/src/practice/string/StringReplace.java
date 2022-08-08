64e2da028e4ae4ba1b6d57b99551ca1172bf7570
jaeyeun95 <jaeyeun95@gmail.com>
2022-08-08 20:18:39 +0900

    StringReplace

--- doItJava/src/practice/string/StringReplace.java
+++ doItJava/src/practice/string/StringReplace.java
@@ -4,6 +4,7 @@ public class StringReplace {
     public static void main(String[] args) {
         String basicDt = "2020-06-05";
         System.out.println("replace : " + basicDt.replace("-",""));
+        
     }
 }
 
